0a03b75a5bbf96e56090ba558fb7270cb424c103
edbert suwandi <esuw5153@uni.sydney.edu.au>
2025-05-10 23:00:19 +1000

    Feature: Basic Webpage Skeleton Code

---
+++ script.js
@@ -0,0 +1,11 @@
+// Function to show student section
+function showStudentSection() {
+    alert("Student section: Here students can sign up to find freelance opportunities.");
+    // Additional student section actions can be added here.
+}
+
+// Function to show client section
+function showClientSection() {
+    alert("Client section: Here clients can post jobs and access student talent.");
+    // Additional client section actions can be added here.
+}
